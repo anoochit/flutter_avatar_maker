af2a5fd8952c5ef3f3dcf69f4ef4626cfb4654f2
Anuchit Chalothorn <anoochit@gmail.com>
2025-08-25 21:47:25 +0700

    update sdk

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_avatar_maker/main.dart';
+import 'package:avatar_maker/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
